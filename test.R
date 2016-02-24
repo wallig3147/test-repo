30387da7740f25753b3ddbea1ab604cb62aabfa7
Robin <rob00x@gmail.com>
2016-02-24 15:02:33 +0000

    Add README and test.R script

---
+++ test.R
@@ -0,0 +1,17 @@
+## Robin
+# Create some imaginary data and plot
+x = seq(0, 10 * pi, length.out = 1000)
+y = sin(x) * exp(-0.1 * x)
+z = sin(x - 2) * exp(-0.06 * x)
+plot(data.frame(x, y, z))
+
+## Andy
+# ... Add your lines here
+
+
+
+
+
+
+
+# Person 3 ...
